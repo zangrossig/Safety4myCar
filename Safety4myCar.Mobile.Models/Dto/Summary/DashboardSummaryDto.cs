b7e4f7e7f3826a10fd1558b7dc76ac363e25d73d
Giovanni Zangrossi <giovanni.zangrossi@plannet.it>
2024-08-08 09:09:13 +0200

    feat: DashboardSummaryDto

---
+++ Safety4myCar.Mobile.Models/Dto/Summary/DashboardSummaryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Safety4myCar.Mobile.Models.Dto.Summary
+{
+	public class DashboardSummaryDto
+	{
+		public Guid AutomezzoId { get; set; }
+		public int NumeroRegistrazioni { get; set; }
+		public decimal ImportoSpese { get; set; }
+		public decimal Consumo { get; set; }
+		public int Percorrenza { get; set; }
+		public decimal SpesaPerKm { get; set; }
+	}
+}
